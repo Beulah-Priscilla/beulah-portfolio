cd3edef1e5a741c3b4abe6d8caca9ad5b78f5aca
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-08-16 13:43:49 -0400

    Wrapped App inside theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react'
+import theme from './theme.ts'
+import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 )
